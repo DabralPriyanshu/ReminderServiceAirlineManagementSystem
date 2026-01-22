86782376ea11093c2726247c06e3be738085d843
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-01-22 12:53:18 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
-const sendBasicMail = require("./services/email-service");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 async function startServer() {
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
-    sendBasicMail(
-      "support@admin.com",
-      "priyanshudabral07@gmail.com",
-      "this is testing email",
-      "hey how are you doing",
-    );
   });
 }
 startServer();
